5ca252ddbbcfcee3939d29f2eee11fdb1b28d80b
Josiah Coad <josiah@coad.net>
2017-04-15 23:15:46 -0500

    edited josiah

---
+++ amazon_parser.py
@@ -0,0 +1,62 @@
+# find out how many groups there that meet the following criteria
+'''
+members reviewed 3 or more of the same products (and no others?)
+members reviewed with same star rating (either 5 or 1 star)
+members posted within 4 days of eachother
+2 or more members
+'''
+
+# score of spamicity could be based on the following
+'''
+>>1. earliness of reviews - Group Early Time Frame (GETF)
+2. group deviation (diff between rating from group and non-group members) (GD)
+>>>3. group content similarity (similarity between all reviews in the group) (GCS)
+4. group member content similarity - for each member in the group, how similar are their own reviews? (GMCS) 
+>>5. time closeness of reviews - g time window (GTW) 
+size of group (normalized)
+'''
+
+'''
+x, y axis of graphs?
+how to determine dimensions for cosine similarity in reviews (length? sentiment? exact word use? syntax? mispelling?)
+if groups are determined by the "only review these products" rule, than there wont be any two groups who
+   share the same reviewers... right?
+'''
+
+
+# returns a list of reviews represented as dict objects.
+# put in a number of lines to read from file 
+# or put in no number and it will read all
+def parserJSON(path, numLines=None):
+   numLines = numLines or len(open(path).read().split("\n")) - 1
+   with open(path) as txt:
+      reviews = [eval(next(txt)) for x in range(numLines)]
+   return reviews
+
+
+
+# create a dict with reviewer ID as key and a list of the reviewers reviews as the value
+def get_reviewers(reviews):
+   reviewers = {}
+   for review in reviews:
+      reviewerId = review["memberId"]
+      if reviewerId not in reviewers:
+         reviewers[reviewerId] = [review]
+      else:
+         reviewers[reviewerId].append(review)
+   final_reviewers = {}
+   for reviewer in reviewers:
+      if len(reviewers[reviewer]) >= 3:
+         final_reviewers[reviewer] = reviewers[reviewer]
+   return final_reviewers
+
+# create a dict with product ID as the key and a list of the product's reviews as the value
+def get_products(reviews):
+   products = {}
+   for review in reviews:
+      productId = review["productId"]
+      if productId not in products:
+         products[productId] = [review]
+      else:
+         products[productId].append(review)
+   return products
--- josiah_work.py
+++ josiah_work.py
@@ -1,9 +1,40 @@
+from __future__ import print_function
 from modules.amazon_parser import *
 
-reviews = parserJSON('./library/amazon-review-data.json', 100)
+reviews = parserJSON('./library/amazon-review-data.json')
 
-# sort dictionary reviews by len of reviews
+# get dictionary with each user as key and a list of their reviews as value
 reviewers = get_reviewers(reviews)
-sorted_reviewers = sorted(reviewers.items(), len(reviewers[key]))
+# change dictionary into list of tuples
+reviewers = reviewers.items()
+# sort reviewers by the number of reviews they left
+reviewers.sort(key=lambda x: len(x[1]))
+# for each reviewer, sort their reviews by order of product ID
+for r in reviewers:
+   # r[0] is the reviewer id
+   # r[1] is the list of their reviews
+   r[1].sort(key=lambda review: review["productId"])
+
+
+# create a list of tuples... with first entry being the reviewer 
+# and second being a concatenated string of the product ids reviewed 
+print()
+# this way, if two reviewers reviewed the same objects, they'll be adjacent
+reviewers_short = []
+for reviewer in reviewers:
+   reviewers_short.append((reviewer[0], "".join([review["productId"] for review in reviewer[1]])))
+# print(*reviewers_short, sep="\n\n")
+
+# compare adjacent reviewers to see if any are the same
+for i in range(len(reviewers_short)-1):
+   if reviewers_short[i][1][:4] == reviewers_short[i+1][1][:4]:
+      print(reviewers_short[i])
+
+
+
+
+
+''' old code '''
+# sorted_reviewers = sorted(reviewers.items(), len(reviewers[key]))
 # busiest = max(reviewers.keys(), key=(lambda key: len(reviewers[key])))
 # print(len(reviewers[busiest]))
