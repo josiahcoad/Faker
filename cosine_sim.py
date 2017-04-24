4fd9e165bfb3b8acd8a34a3dab65ad8acb68b1de
Josiah Coad <josiah@coad.net>
2017-04-23 19:23:19 -0500

    some additional group indicators

--- cosine_sim.py
+++ cosine_sim.py
@@ -8,5 +8,10 @@ def cosine_sim(string1, string2):
    for word in string2:
       count2[word.lower()] += 1
    dot_product = sum(count1.get(key, 0)*count2.get(key, 0) for key in count1)
-   magnitude = math.sqrt(sum([val**2 for key, val in count1.items()])) * math.sqrt(sum([val**2 for key, val in count2.items()]))
+   magnitude = math.sqrt(sum([int(val)**2 for val in count1.values()])) * math.sqrt(sum([int(val)**2 for val in count2.values()]))
    return dot_product/magnitude if magnitude else 0
+
+if __name__ == "__main__":
+   R1 = "I am using this  product for  more than  3 weeks and  it is working great on me . I had lot of hair falls and used many products before to stop the hair fall but was in vain .I did not know what  to do , then I got  to know about this  product from my close  relative.  I bought this & after using it for few weeks , it started  to show results -  I was so happy that  my hair fall stopped and my hair became thick . Highly recommendable to everyone who has hairfall problem."
+   R2 = "supplement has done me nothing else but the good. Not only has it worked effectively for me but also for my friends. The results have been nothing less than tender skin, strong nails and hair. It also helps boost my energy which keeps me going strong the entire day. I indorse it."
+   print (cosine_sim(R1, R2))
--- group_analysis.py
+++ group_analysis.py
@@ -2,39 +2,44 @@ from __future__ import print_function
 from collections import defaultdict
 from cosine_sim import cosine_sim
 from numpy import mean as avg
+from modules.amazon_parser import *
+
+reviews = parserJSON('./library/amazon-review-data.json')
+products_dict = get_products(reviews)
+
 with open("./library/groups.txt") as f:
    groups = eval(f.read())
 
-# takes a group which is organized by reviewer_reviews and translates it to product_reviews
-def translate_group(group):
-   reviews = []
-   for user_reviews in group:
-      reviews.extend(user_reviews[1])
-   products_reviews = defaultdict(list)
-   for review in reviews:
-      products_reviews[review["productId"]].append(review)
-   return products_reviews.items()
+# takes a dictionary of groups which are organized by groupID as the key and a list of tuples as the value
+# returns a list of a list of tuples where internal list is a group and each tuple inside that group is (product, [reviews])
+def organize_by_product(groups_dict):
+   group_list = []
+   for groupId, group in groups_dict.items():
+      reviews = []
+      for user, user_reviews in group:
+         reviews.extend(user_reviews)
+      products_reviews = defaultdict(list)
+      for review in reviews:
+         products_reviews[review["productId"]].append(review)
+      group_list.append( products_reviews.items() )
+   return group_list
 
-
-
-MAXTIME = 345600 # number of seconds in 4 days
+GTW_MAXTIME = 345600 # number of seconds in 4 days
 def GTW(group):
-   products_reviews = translate_group(group)
    timewindows = []
-   for product_reviews in products_reviews:
-      timestamps = [float(review["Date"]) for review in product_reviews[1]]
+   for product, reviews in group:
+      timestamps = [float(review["Date"]) for review in reviews]
       timewindows.append(prod_TW(timestamps))
    return max(timewindows)
 
 def prod_TW(timestamps):
    _range = max(timestamps)-min(timestamps)
-   return 1-_range/MAXTIME if _range < MAXTIME else 0
+   return 1-_range/GTW_MAXTIME if _range < GTW_MAXTIME else 0
 
 def GCS(group):
-   products_reviews = translate_group(group)
    cs = []
-   for product_reviews in products_reviews:
-      reviews = [review["reviewText"] for review in product_reviews[1]]
+   for product, reviews in group:
+      reviews = [review["reviewText"] for review in reviews]
       cs.append(CS(reviews))
    return max(cs)
 
@@ -45,12 +50,33 @@ def CS(reviews):
          cs.append(cosine_sim(review, review2))
    return avg(cs)
 
-for groupId, group in groups.items():
-   print(GCS(group))
+# def GETF(group):
+#    products_reviews = translate_group(group)
+#    gtf = []
+#    for product, reviews in products_reviews:
+#       timestamps = [float(review["Date"]) for review in reviews]
+#       earliest_time_for_product = min([float(review["Date"]) for review in products_dict[product]])
+#       gtf.append(GTF(timestamps, earliest_time))
+#    return max(gtf)
+
+def GETF(group):
+   return max([GTF(product, reviews) for product, reviews in group])
+
+GTF_MAXTIME = 15552000 # seconds in 6 months
+def GTF(product, reviews):
+   earliest_product_review = min([float(review["Date"]) for review in products_dict[product]])
+   latest_group_review = max([float(review["Date"]) for review in reviews])
+   _range = latest_group_review-earliest_product_review
+   return 1-_range/GTF_MAXTIME if _range < GTF_MAXTIME else 0
+   
+groups = organize_by_product(groups)
+for group in groups:
+   print(GETF(group))
+
 
 
 # print sz of groups
-sz = defaultdict(int)
-for groupId, group in groups.items():
-   sz[len(group)] += 1
-print(sz)
+# sz = defaultdict(int)
+# for groupId, group in groups.items():
+#    sz[len(group)] += 1
+# print(sz)
