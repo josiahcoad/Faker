2b01b3c2b5a6f9cd0e5105c10dcc9ca55ba51a8c
Sheelabhadra Dey <sheelabhadra@gmail.com>
2017-05-01 15:48:48 -0500

    SOM to find fake group IDs

---
+++ SOM_minisom.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+from SOM_Trianing import som
+import numpy as np
+from group_analysis_jo import get_all_scores
+from group_analysis_jo import get_reviewer_groups
+from grouping2 import group_users
+from modules.amazon_parser import *
+with open("./library/groups.txt") as f:
+    groups = eval(f.read())
+
+X = np.array(get_all_scores())
+
+# Training the SOM
+# We will use MiniSom 1.0
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 9, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100000)
+
+som_distance = som.distance_map().T
+#print('som map', som_distance)
+
+# Detecting the fake groups and reviewers
+mappings = som.win_map(X)
+print('size of the SOM map', len(mappings))
+#text_file = open("mappings.txt", "w")
+#text_file.write("winning map is %s" % mappings)
+#text_file.close()
+
+# List of fake review group IDs
+faker_list = []
+for i in xrange(len(som_distance)):
+	for j in xrange(len(som_distance[0])):
+		if som_distance[i][j] > 0.8:
+			fake_groups = mappings[(i,j)]
+			for group in fake_groups:
+				fake_group_ID = group[0]
+				faker_list.append(fake_group_ID)
+print('list of fake group IDs is\n', faker_list)
+
+'''
+# List of fake reviewer IDs
+# get the list of fake reviewers
+fake_reviewers = group_users()
+for g in faker_list:
+	set(g[])
+set()
+'''
+
+# Visualizing the results (SOM in a 2-D plot)
+from pylab import bone, pcolor, colorbar, plot, show
+bone() # creates a white window
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+show()
+
